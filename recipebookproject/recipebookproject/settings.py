111c3608a10cd62086b5a549a62c1f0151acfd09
RASHID SHARAFUTDINOV <144220001+ShRashid@users.noreply.github.com>
2025-02-12 11:22:01 +0300

    Update doc

--- recipebookproject/recipebookproject/settings.py
+++ recipebookproject/recipebookproject/settings.py
@@ -39,9 +39,7 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
-    '192.168.1.10',
-    '127.0.0.1',
-    'andybook.pythonanywhere.com',
+    '*',
 ]
 
 # Application definition
